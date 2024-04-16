621f0f96d0c0b0200669c75d9b0c137ffcdef5c5
poula9744 <poula9744@naver.com>
2024-04-16 22:06:27 +0900

    ㅇㅇㅂ

--- src/main/java/com/javaex/service/PaymentService.java
+++ src/main/java/com/javaex/service/PaymentService.java
@@ -1,9 +1,12 @@
 package com.javaex.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javaex.dao.PaymentDao;
+import com.javaex.vo.OrdersVo;
 
 @Service
 public class PaymentService {
@@ -11,5 +14,12 @@ public class PaymentService {
 	@Autowired
 	private PaymentDao paymentDao;
 
+	public List<OrdersVo> exePaymentManageList() {
+		System.out.println("exePaymentManagerList()");
+		
+		List<OrdersVo> paymentManageList = paymentDao.paymentManageList();
+		
+		return paymentManageList;
+	}
 	
 }
